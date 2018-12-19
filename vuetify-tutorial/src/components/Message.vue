2056d947bf1861709e41a3442d743b5dbc1f5239
asuforce <owata.sn@gmail.com>
2018-12-19 16:40:11 +0900

    Display real time

--- vuetify-tutorial/src/components/Message.vue
+++ vuetify-tutorial/src/components/Message.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
+    <v-flex xs12>
       <v-card>
-        <v-img class="white--text" height="200px" v-bind:src="main">
+        <v-img class="white--text" height="800px" v-bind:src="main">
           <v-container fill-height fluid>
             <v-layout fill-height>
               <v-flex xs12 align-end flexbox>
@@ -13,7 +13,7 @@
         </v-img>
         <v-card-title>
           <div>
-            <span class="grey--text">November 11</span>
+            <span class="grey--text">{{now()}}</span>
             <br>
             <span>Basta</span>
             <br>
@@ -26,10 +26,15 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   data: () => ({
     main:
       "https://66.media.tumblr.com/5c13dbb6b5275eedce4cd21a762b31de/tumblr_pi4twh9GNl1v1tcqmo1_1280.jpg"
   }),
+  methods: {
+    now: () => moment(new Date()).format("MMMM Do YYYY, h:mm:ss a")
+  }
 };
 </script>
