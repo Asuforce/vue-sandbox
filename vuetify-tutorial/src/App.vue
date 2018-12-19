8be89345da8fd0a88bbff753505306b3baabc798
asuforce <owata.sn@gmail.com>
2018-12-19 15:15:37 +0900

    Create gallary

--- vuetify-tutorial/src/App.vue
+++ vuetify-tutorial/src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+    <v-toolbar>
+      <v-toolbar-title>Asuforce gallery</v-toolbar-title>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <main>
+      <v-container fluid>
+        <Gallary/>
+        <router-view></router-view>
+      </v-container>
+    </main>
+    <v-footer>Design by Asuforce.</v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Gallary from "./components/Gallary";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Gallary
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
