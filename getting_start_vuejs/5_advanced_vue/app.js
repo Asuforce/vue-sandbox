5ba9a545302122c607fd3fb11fb0ff8ea4a23ecf
asuforce <owata.sn@gmail.com>
2019-02-13 18:22:59 +0900

    5.1.4 Javascript hook

--- getting_start_vuejs/5_advanced_vue/app.js
+++ getting_start_vuejs/5_advanced_vue/app.js
@@ -1,8 +1,41 @@
-new Vue({
-  el: '#app',
-  data() {
+var PullDownMenu = {
+  data: function() {
     return {
-      isShown: false
-    }
+      isShown: false,
+      name: "Menu",
+      items: ["1-1", "1-2", "1-3"]
+    };
   },
-})
+  template: "#pull-down-menu",
+  methods: {
+    beforeEnter: function(el) {
+      el.style.height = "0px";
+      el.style.opacity = "0";
+    },
+    enter: function(el, done) {
+      anime({
+        targets: el,
+        opacity: 1,
+        height: el.scrollHeight + "px",
+        duration: 3000,
+        complete: done
+      });
+    },
+    leave: function(el, done) {
+      anime({
+        targets: el,
+        opacity: 0,
+        height: "0px",
+        duration: 300,
+        complete: done
+      });
+    }
+  }
+};
+
+new Vue({
+  el: "#app",
+  components: {
+    PullDownMenu: PullDownMenu
+  }
+});
