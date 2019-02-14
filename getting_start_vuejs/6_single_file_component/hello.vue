3f0d0b00e80188528ec2176f7be86f1f63e5fb28
asuforce <owata.sn@gmail.com>
2019-02-14 16:58:02 +0900

    Create 6th dir

---
+++ getting_start_vuejs/6_single_file_component/hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <p clss="message">Message: {{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return { msg: "Hello, world!" };
+  }
+};
+</script>
+
+<style>
+.message {
+  color: #42b983;
+}
+</style>
