faa2cfa45d87151f183a68dcb3d1cb3d72d21bc9
asuforce <owata.sn@gmail.com>
2019-02-14 12:37:51 +0900

    5.2.3 Slot scope

--- getting_start_vuejs/5_2_slot/app.js
+++ getting_start_vuejs/5_2_slot/app.js
@@ -1,10 +1,26 @@
-var MyPage = {
-  template: "#my-page"
+var TodoList = {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  template: "#todo-list"
 };
 
 new Vue({
-  el: '#app',
+  el: "#app",
+  data: function() {
+    return {
+      todos: [
+        { id: 1, text: "C++", isCompleted: true },
+        { id: 2, text: "JavaScript", isCompleted: false },
+        { id: 3, text: "Java", isCompleted: true },
+        { id: 4, text: "Perl", isCompleted: false }
+      ]
+    };
+  },
   components: {
-    MyPage: MyPage
+    TodoList: TodoList
   }
-})
+});
