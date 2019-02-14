e621ea4a565d1ba8ca32bbcd11e8639e258fb05e
asuforce <owata.sn@gmail.com>
2019-02-14 18:27:17 +0900

    7.5.2 List tasks

--- getting_start_vuejs/7_vuex/App.vue
+++ getting_start_vuejs/7_vuex/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Task list</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    }
+  }
+};
 </script>
 
--- getting_start_vuejs/7_vuex/store.js
+++ getting_start_vuejs/7_vuex/store.js
@@ -3,6 +3,21 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({});
+const store = new Vuex.Store({
+  state: {
+    tasks: [
+      {
+        id: 1,
+        name: "Buy milk",
+        done: false
+      },
+      {
+        id: 2,
+        name: "Buy Vue's book",
+        done: false
+      }
+    ]
+  }
+});
 
 export default store;
