5f1421b557ab68bcffd62bd0cecf10ec1620d9e5
asuforce <owata.sn@gmail.com>
2019-02-14 19:02:01 +0900

    7.5.4 Implement label function

--- getting_start_vuejs/7_vuex/App.vue
+++ getting_start_vuejs/7_vuex/App.vue
@@ -5,11 +5,27 @@
       <li v-for="task in tasks" v-bind:key="task.id">
         <input type="checkbox" v-bind:checked="task.done" v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
+        <span
+          v-for="id in task.labels"
+          v-bind:key="id"
+        >{{ getLabelText(id) }}</span>
       </li>
     </ul>
 
     <form v-on:submit.prevent="addTask">
       <input type="text" v-model="newTaskName" placeholder="New task">
+    </form>
+
+    <h2>Label list</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input type="checkbox" v-bind:value="label.id" v-model="newTaskLabelIds">
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addLabel">
+      <input type="text" v-model="newLabelText" placeholder="New label">
     </form>
   </div>
 </template>
@@ -18,25 +34,42 @@
 export default {
   data() {
     return {
-      newTaskName: ""
+      newTaskName: "",
+      newTaskLabelIds: [],
+      newLabelText: ""
     };
   },
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    },
+    labels() {
+      return this.$store.state.labels;
     }
   },
   methods: {
     addTask() {
       this.$store.commit("addTask", {
-        name: this.newTaskName
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
       });
       this.newTaskName = "";
+      this.newLabelText = "";
     },
     toggleTaskStatus(task) {
       this.$store.commit("toggleTaskStatus", {
         id: task.id
       });
+    },
+    addLabel() {
+      this.$store.commit('addLabel', {
+        text: this.newLabelText
+      })
+      this.newLabelText = ""
+    },
+    getLabelText(id) {
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label ? label.text: ''
     }
   }
 };
--- getting_start_vuejs/7_vuex/store.js
+++ getting_start_vuejs/7_vuex/store.js
@@ -9,34 +9,60 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: "Buy milk",
+        labels: [1, 2],
         done: false
       },
       {
         id: 2,
         name: "Buy Vue's book",
+        labels: [1, 3],
         done: false
       }
     ],
+    labels: [
+      {
+        id: 1,
+        text: "Shopping"
+      },
+      {
+        id: 2,
+        text: "Foods"
+      },
+      {
+        id: 3,
+        text: "Books"
+      }
+    ],
     nextTaskId: 3,
+    nextLabelId: 4
   },
   mutations: {
-    addTask(state, { name }) {
+    addTask(state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false
-      })
+      });
 
-      state.nextTaskId++
+      state.nextTaskId++;
     },
     toggleTaskStatus(state, { id }) {
       const filtered = state.tasks.filter(task => {
-        return task.id === id
-      })
+        return task.id === id;
+      });
 
       filtered.forEach(task => {
-        task.done = !task.done
-      })
+        task.done = !task.done;
+      });
+    },
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLavelId,
+        text
+      });
+
+      state.nextLavelId++;
     }
   }
 });
